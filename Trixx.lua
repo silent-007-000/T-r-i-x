ae8fba71a0bf53fcf085f0bff86018b032d4e028
007 <hjdd4xce@gmail.com>
2025-10-19 12:28:29 -0700

    Add Trixx.lua loading script to README

---
+++ Trixx.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/silent-007-000/T-r-i-x/refs/heads/main/O.lua))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/silent-007-000/T-r-i-x/refs/heads/main/Trix.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/silent-007-000/T-r-i-x/refs/heads/main/Curser.lua'))()    
